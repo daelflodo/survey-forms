d225261495b4997bd0d20e0d81a5b7fba8dff1b7
daelflodo <davidflodo@gmail.com>
2023-09-24 14:42:03 -0500

    readme

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Bienvenidos al Server de App Survey, \n https://survey-form-eo0g.onrender.com/documentation';
+    return 'Bienvenidos al Server de App Survey';
   }
 }
