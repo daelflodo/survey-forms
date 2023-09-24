e64d078d77bddcd6d1fd4440e40c276129920527
daelflodo <davidflodo@gmail.com>
2023-09-24 13:57:06 -0500

    feat swager

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Bienvenidos al Server de App Survey, \n https://survey-form-eo0g.onrender.com/documentation';
   }
 }
